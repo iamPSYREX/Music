ab63744e0b3be681a74e54720f45b69ffb9f7649
Hunter XDD <85411336+Hunter-XDD@users.noreply.github.com>
2021-10-05 19:44:04 +0530

    umm

--- Music/Plugins/ping.py
+++ Music/Plugins/ping.py
@@ -5,7 +5,7 @@ import os
 import psutil
 import time
 from datetime import datetime
-from ..MusicUtilities.helpers.time import get_readable_time
+from Music.MusicUtilities.helpers.time import get_readable_time
 
 async def bot_sys_stats():
     bot_uptime = int(time.time() - Music_START_TIME)
