9a76f1947c7c48d4e08e8ab33143f434b1e4b322
Hunter XDD <85411336+Hunter-XDD@users.noreply.github.com>
2021-10-05 19:05:33 +0530

    umm

--- Music/MusicUtilities/tgcallsrun/__init__.py
+++ Music/MusicUtilities/tgcallsrun/__init__.py
@@ -1,9 +1,9 @@
 from os import listdir, mkdir
 from pyrogram import Client
 from Music import config
-from Music.MusicUltilities.queues import (clear, get, is_empty, put, task_done)
-from Music.MusicUltilities.downloader import download
-from Music.MusicUltilities.convert import convert
-from Music.MusicUltilities.music import run
-from Music.MusicUltilities.music import smexy as ASS_ACC
+from Music.MusicUtilities.tgcallsrun.queues import (clear, get, is_empty, put, task_done)
+from Music.MusicUtilities.tgcallsrun.downloader import download
+from Music.MusicUtilities.tgcallsrun.convert import convert
+from Music.MusicUtilities.tgcallsrun.music import run
+from Music.MusicUtilities.tgcallsrun.music import smexy as ASS_ACC
 smexy = 1
