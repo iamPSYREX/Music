e36f601d4e09b2c9ce2fe394c91e2fbf04d583c3
Hunter XDD <85411336+Hunter-XDD@users.noreply.github.com>
2021-10-07 00:25:53 +0530

    umm

--- Music/Plugins/song.py
+++ Music/Plugins/song.py
@@ -120,7 +120,7 @@ async def mpthree(_, message: Message):
             ID5 = (result[4]["id"])
         except Exception as e:
             return await mystic.edit_text(f"Soung Not Found.\n**Possible Reason:**{e}")
-        thumb ="cache/Result.JPEG"
+        thumb ="cache/Results.png"
         await mystic.delete()   
         buttons = search_markup(ID1, ID2, ID3, ID4, ID5, duration1, duration2, duration3, duration4, duration5, user_id, query)
         hmo = await message.reply_photo(
@@ -300,4 +300,4 @@ def gets(videoid, user_id):
                 InlineKeyboardButton(text="🗑 Close Menu", callback_data=f'close2')
             ],
         ]
-    return buttons 
+    return buttons
