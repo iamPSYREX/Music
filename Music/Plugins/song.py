aae5543c1473428a57008afd601fb4d7e2e55c24
Hunter XDD <85411336+Hunter-XDD@users.noreply.github.com>
2021-11-13 17:19:07 +0530

    umm

--- Music/Plugins/song.py
+++ Music/Plugins/song.py
@@ -263,9 +263,9 @@ def search_markup(ID1, ID2, ID3, ID4, ID5, duration1, duration2, duration3, dura
             ],
             [ 
                 
-                InlineKeyboardButton(text="<", callback_data=f'chonga 1|{query}|{user_id}'), 
+                InlineKeyboardButton(text="⬅️", callback_data=f'chonga 1|{query}|{user_id}'), 
                 InlineKeyboardButton(text="🗑 Close", callback_data=f"ppcl2 smex|{user_id}") ,
-                InlineKeyboardButton(text=">", callback_data=f'chonga 1|{query}|{user_id}')             
+                InlineKeyboardButton(text="➡️", callback_data=f'chonga 1|{query}|{user_id}')             
             ],
         ]
     return buttons   
@@ -283,9 +283,9 @@ def search_markup2(ID6, ID7, ID8, ID9, ID10, duration6, duration7, duration8, du
             ],
             [ 
                 
-                InlineKeyboardButton(text="<", callback_data=f'chonga 2|{query}|{user_id}'), 
+                InlineKeyboardButton(text="⬅️", callback_data=f'chonga 2|{query}|{user_id}'), 
                 InlineKeyboardButton(text="🗑 Close", callback_data=f"ppcl2 smex|{user_id}") ,
-                InlineKeyboardButton(text=">", callback_data=f'chonga 2|{query}|{user_id}')             
+                InlineKeyboardButton(text="➡️", callback_data=f'chonga 2|{query}|{user_id}')             
             ],
         ]
     return buttons     
